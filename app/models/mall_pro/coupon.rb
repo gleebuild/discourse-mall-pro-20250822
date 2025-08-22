dd551e702f94b8df026707c65ad029f3367b8f7d
gleebuild <gleebuild@gmail.com>
2025-08-22 10:36:22 +0800

    Delete app directory

--- app/models/mall_pro/coupon.rb
+++
@@ -1,9 +0,0 @@
-
-# frozen_string_literal: true
-module MallPro
-  class Coupon < ::ActiveRecord::Base
-    self.table_name = "mall_coupons"
-    validates :code, presence: true, uniqueness: true
-    enum discount_type: {{ percent: 0, amount: 1 }}
-  end
-end
