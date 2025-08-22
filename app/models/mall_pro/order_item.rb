85731d2355215bed9f7348c583720696c549b59a
gleebuild <gleebuild@gmail.com>
2025-08-22 10:42:22 +0800

    Delete app directory

--- app/models/mall_pro/order_item.rb
+++
@@ -1,9 +0,0 @@
-
-# frozen_string_literal: true
-module MallPro
-  class OrderItem < ::ActiveRecord::Base
-    self.table_name = "mall_order_items"
-    belongs_to :order, class_name: "MallPro::Order"
-    belongs_to :product, class_name: "MallPro::Product"
-  end
-end
